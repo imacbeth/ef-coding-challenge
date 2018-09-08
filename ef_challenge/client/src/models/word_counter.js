fbe6fa675f7cefa1d6f9a10ff03de16700822a55
Iona Macbeth <ionalmacbeth@gmail.com>
2018-09-08 16:07:45 +0100

    initial commit

---
+++ ef_challenge/client/src/models/word_counter.js
@@ -0,0 +1,41 @@
+const WordCounter = function(text) {
+  this.text = text;
+};
+
+WordCounter.prototype.createArrayOfWords = function (text) {
+  const wordsArray = text.split(/\s+/);
+  return wordsArray;
+};
+
+WordCounter.prototype.createWordMap = function (wordsArray) {
+  const wordsMap = {};
+
+  wordsArray.forEach(function (key) {
+    if (wordsMap.hasOwnProperty(key)) {
+      wordsMap[key]++;
+    } else {
+      wordsMap[key] = 1;
+    }
+  });
+
+  return wordsMap;
+};
+
+
+WordCounter.prototype.sortByFrequency = function (wordsMap) {
+  const finalWordsArray = [];
+  finalWordsArray = Object.keys(wordsMap).map(function(key) {
+    return {
+      name: key,
+      total: wordsMap[key]
+    };
+  });
+// to sort in descending order
+  finalWordsArray.sort(function(a, b) {
+    return b.total - a.total;
+  });
+
+  return finalWordsArray;
+};
+
+module.exports = WordCounter;
